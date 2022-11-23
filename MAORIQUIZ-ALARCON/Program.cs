f58932c958e78fe5972e3408913cf1e7f00c7fb3
Challee Alarcon <ac151535@avcol.school.nz>
2022-11-23 14:14:13 +1300

    added comments about the structure of the whole program

--- MAORIQUIZ-ALARCON/Program.cs
+++ MAORIQUIZ-ALARCON/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+{
+    /*declare play() so that the method returns to no value
+     * ask for the user's name
+     * should display an introduction with the user's name
+     * ask user for which level
+     * display total score of user
+     * if users score less than or equal to 5, display a particular message
+     * if users score more than or equal to 6, display a particular message
+     * if users score less than or uqual to 20, display a particular message
+     * if users score more than or equal to 21, display a particular message
+     * display outro message and ask user if they want to play again
+     * if they want to play again, they can press s 
+     * if they don't want to play anymore, they can press any key to end the quiz 
+     * thank the user for using this program
+     */
+
+    play();
+}
+static void play()
+{
+    string answer = "";
+
+    int point = 0;
+
+    Console.Clear();
+
+}
